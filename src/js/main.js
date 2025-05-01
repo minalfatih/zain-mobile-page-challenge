3148ab964fb3825f5921f86c1b8519752c5d3951
minalfatih220@gmail.com <minalfatih220@gmail.com>
2025-05-01 15:09:52 +0200

    Update files

--- src/js/main.js
+++ src/js/main.js
@@ -4,30 +4,31 @@ document.querySelectorAll(".box p").forEach((el) => {
   }
 });
 
-/*document.querySelectorAll(".audio").forEach((el) => {
-  el.addEventListener("click", function () {
-    if (
-      document.querySelector(".audio .sheiks-info span").dataset.name ===
-      "osmanAbubakr"
-    ) {
-      document.getElementById("play").play();
-      document.querySelector(".playAudio").classList.add("displed");
-      document.querySelector(".stop").classList.add("active");
-    }
-  });
-});*/
+let audio = document.getElementById("myAudio");
+let playButton = document.querySelector(".playButton");
+let playIconPath = document.querySelector(".playButton svg path");
+let isPlaying = false;
 
+playButton.addEventListener("click", function () {
+  if (isPlaying) {
+    audio.pause();
+    playIconPath.setAttribute(
+      "d",
+      "M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"
+    );
+    isPlaying = false;
+  } else {
+    audio.play();
+    playIconPath.setAttribute(
+      "d",
+      "M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"
+    );
+    isPlaying = true;
+  }
+});
+
+// heart
 document.addEventListener("click", function (e) {
-  if (e.target.classList.contains("playAudio")) {
-    document.querySelector(".playAudio").classList.add("displed");
-    document.querySelector(".stop").classList.add("active");
-    document.getElementById("play").play();
-  }
-  if (e.target.classList.contains("stop")) {
-    document.querySelector(".playAudio").classList.remove("displed");
-    document.querySelector(".stop").classList.remove("active");
-    document.getElementById("play").pause();
-  }
   if (e.target.classList.contains("heart")) {
     if (document.querySelector(".heart").classList.contains("active")) {
       document.querySelector(".heart").classList.remove("active");
@@ -81,3 +82,16 @@ function slide() {
 }
 
 slide();
+
+// progress
+let progressBar = document.getElementById("progressBar");
+audio.addEventListener("timeupdate", function () {
+  if (audio.duration) {
+    let progress = (audio.currentTime / audio.duration) * 100;
+    progressBar.value = progress;
+  }
+});
+
+audio.addEventListener("ended", function () {
+  progressBar.value = progressBar.max;
+});
